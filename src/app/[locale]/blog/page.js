930aa71af8541c5ae71cd788967fba6e74787d7a
Kapacioc <andreigherghevici@gmail.com>
2024-12-06 17:02:01 +0200

    06.12.2024

--- src/app/[locale]/blog/page.js
+++ src/app/[locale]/blog/page.js
@@ -1,33 +1,34 @@
 import ImgCard from "../components/ImgCard"
+import img1 from "../../../../public/images/image01.png"
+import img2 from "../../../../public/images/image02.png"
 
-export default function Blog(){
-    const data = [
-        {
-            "img":"",
-            "title":"test",
-            "description":"dadadad"
-        },
-        {
-            "img":"...",
-            "title":"test2",
-            "description":"dadadad"
-        },
-        {
-            "img":"...",
-            "title":"test3",
-            "description":"dadadad"
-        },
-        {
-            "img":"...",
-            "title":"test4",
-            "description":"dadadad"
-        }
-    ]
+import {setRequestLocale} from 'next-intl/server';
+import { useTranslations } from "next-intl";
+export default function Blog({params}){
+    
+    async function loc(){
+        const {locale} = await params;
+        return locale
+      }
+      setRequestLocale(loc());
+      const t = useTranslations("Blog")
+
     return(
-        <main className="px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60 py-20 flex flex-col justify-between " >
-            {data.map((val)=>{
-                return <ImgCard key={val?.title} title={val?.title} description={val?.description} img={val?.img}/>
-            })}
-        </main >
+        <>
+            <section className="pt-20 pb-10 bg-lime-900 text-white/90 px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60">
+                <h1 className="text-center text-4xl md:text-5xl font-extrabold">{t("section1.h1")}</h1>
+                <p className="text-center text-sm md:text-base pt-5">{t("section1.p")}</p>
+            </section>
+
+            <main className="px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60 py-20 flex flex-col gap-5  " >
+                    <div className="flex justify-center md:justify-start ">
+                        <ImgCard title={t("cards.card1.h2")} description={t("cards.card1.p")} img={img1} isPhotoRight={true}/>
+                    </div>
+                    <div className=" flex justify-center md:justify-end">
+                        <ImgCard title={t("cards.card2.h2")} description={t("cards.card2.p")} img={img2} isPhotoRight={false}/>
+                    </div>
+            </main>
+        </>
+       
     )
 }
--- src/app/[locale]/components/ImgCard.js
+++ src/app/[locale]/components/ImgCard.js
@@ -1,14 +1,25 @@
 import Image from "next/image";
-export default function ImgCard({title,description,img}){
+import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowBack } from "react-icons/io";
+export default function ImgCard({title,description,img, isPhotoRight}){
     return(
-        <main>
-            <div>
-                {/* <Image src={""} alt="img"/> */}
+        <main className={`flex flex-col sm:flex-row rounded-2xl border-2 border-lime-900 h-fit w-fit `}>
+            <div className={` ${isPhotoRight?"md:order-1":"md:order-2"} text-center  p-2 sm:p-4 w-80 sm:w-[23rem] flex flex-col items-center justify-around`}>
+                <h2 className="text-2xl sm:text-3xl ">{title}</h2>
+                <p className="text-sm text-black/80">{description}</p>
             </div>
-            <div>
-                <h2>{title}</h2>
-                <p>{description}</p>
+            <div className={`${isPhotoRight?"md:order-2":"md:order-1"} flex flex-col gap-2 justify-center items-center  sm:py-4 w-80 sm:w-56 relative `}>
+               <Image src={img} alt="img" className="rounded-xl object-scale-down w-[200px] h-[200px]"  /> 
+               <button className={` sm:absolute w-full sm:w-16 h-8 sm:h-14  bg-amber-200 ${isPhotoRight? "sm:right-0 sm:rounded-tl-[100%] sm:rounded-br-2xl sm:border-l-2":"sm:right-0 sm:rounded-tl-[100%] sm:rounded-br-2xl sm:border-l-2 md:rounded-tl-none md:rounded-br-none md:border-l-0 md:left-0 md:rounded-tr-[100%] md:rounded-bl-2xl md:border-r-2"} sm:bottom-0  rounded-b-2xl sm:rounded-b-none   flex justify-center items-center border-t-2  border-lime-900 `}>
+                    {isPhotoRight?
+                        <IoIosArrowForward className="ml-2 mt-1"/>
+                        :
+                        <IoIosArrowBack className="rotate-180 md:rotate-0 ml-2 md:ml-0 md:mr-2 mt-1"/>
+                    }
+                
+                </button>
             </div>
+            
 
         </main>
     )
