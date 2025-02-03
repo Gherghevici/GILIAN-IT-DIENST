bcea324051aa448fba2a20c81f76a8e76c1caf39
GYulius <giulianbqc@gmail.com>
2025-02-03 17:01:54 +0200

    stories

--- src/app/[locale]/about/page.js
+++ src/app/[locale]/about/page.js
@@ -1,7 +1,53 @@
-export default function About(){
-    return(
-        <main>
-                about
-        </main>
-    )
-}
+    import ImgCard from "../components/ImgCard"
+    import img3 from "../../../../public/images/image03-7.png"
+    import img4 from "../../../../public/images/image04-5.png"
+    import img5 from "../../../../public/images/image03.png"
+    import img6 from "../../../../public/images/image04.png"
+    
+    import {setRequestLocale} from 'next-intl/server';
+    import { useTranslations } from "next-intl";
+    export default function Blog({params}){
+        
+        //export default function About(){
+        //    return(
+       //         <main>
+        //                about
+         //       </main>
+         //   )
+
+        async function loc(){
+            const {locale} = await params;
+            return locale
+          }
+          setRequestLocale(loc());
+          const t = useTranslations("About")
+    
+        return(
+            <>
+                <section className="pt-20 pb-10 bg-lime-900 text-white/90 px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60">
+                    <h1 className="text-center text-4xl md:text-5xl font-extrabold">{t("section1.h1")}</h1>
+                    <p className="text-center text-sm md:text-base pt-5">{t("section1.p")}</p>
+                </section>
+    
+                <main className="px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60 py-20 flex flex-col gap-5  " >
+                        <div className="flex justify-center md:justify-start ">
+                            <ImgCard title={t("cards.card1.h2")} description={t("cards.card1.p")} img={img3} isPhotoRight={true}/>
+                        </div>
+                        <div className=" flex justify-center md:justify-end">
+                            <ImgCard title={t("cards.card2.h2")} description={t("cards.card2.p")} img={img4} isPhotoRight={false}/>
+                        </div>
+                </main>
+
+                <section className="pt-20 pb-10 bg-lime-900 text-white/90 px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60">
+                    <h1 className="text-center text-4xl md:text-5xl font-extrabold">{t("section2.h1")}</h1>
+                    <p className="text-center text-sm md:text-base pt-5">{t("section2.p")}</p>
+                </section>
+    
+                <main className="px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60 py-20 flex flex-col gap-5  " >
+                        <div className="flex justify-center md:justify-start ">
+                            <ImgCard title={t("cards.card1.h2")} description={t("cards.card1.p")} img={img5} isPhotoRight={false}/>
+                        </div>
+            </>
+           
+        )
+    }
--- src/app/[locale]/components/Header.js
+++ src/app/[locale]/components/Header.js
@@ -3,13 +3,15 @@ import { HiBars3CenterLeft } from "react-icons/hi2";
 import { IoCloseOutline } from "react-icons/io5";
 import {Link} from "@/i18n/routing";
 import LanguageSwitcher from "./LanguageSwitcher";
+import ImgCard from "./ImgCard"
+import logo from "../../../../public/images/gilian50.png"
 
 
 export default  function Header({home,about,blog,careers,contact}){
     
     return(
        <header className="flex justify-between z-50 px-5 md:px-10 lg:px-20 xl:px-32 2xl:px-60 py-2 fixed w-full bg-white/95 border-b-4 border-lime-900 rounded-b-3xl md:rounded-b-full ">
-            <div className="order-2 basis-1/2 text-center md:order-1 md:text-left md:basis-1/4 ">{/*<Image src={test} alt="hm" className="h-[32px] object-contain"/>*/}LOGO</div>
+            <div className="order-2 basis-1/2 text-center md:order-1 md:text-left md:basis-1/4 ">{/*<Image src={"../../../public/images/gilian50.png"} alt="Logo" className="h-[32px] object-contain"/>*/}logo</div>
             <div className="basis-1/3 sm:basis-1/4  order-1 md:order-2 md:basis-1/2 peer group">
                 <input type="checkbox" id="nav" className="hidden"/>
                 <label htmlFor="nav" className="peer-has-[input:checked]:block hidden md:hidden"><IoCloseOutline  className="w-8 h-8"/></label>
